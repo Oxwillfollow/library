a65ee2533e4d14b02ec1c6fdf71fe3504d1c0cda
Oxwillfollow <240659154+Oxwillfollow@users.noreply.github.com>
2025-12-02 12:32:18 +0200

    Add a table to show the library and add remove button and checkmark to change the array items

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const inputGenre = document.getElementById('genre');
 const inputYear = document.getElementById('year');
 const inputPages = document.getElementById('pages');
 const inputRead = document.getElementById('read');
+const libraryTable = document.querySelector('.library-table');
+const libraryTbody = libraryTable.querySelector('tbody');
 
 newBookBtn.addEventListener('click', () => newBookDialog.showModal());
 cancelBtn.addEventListener('click', () => newBookDialog.close());
@@ -19,22 +21,70 @@ newBookDialog.addEventListener('close', () => {
     inputAuthor.value = '';
     inputGenre.value = '';
     inputYear.value = '';
+    inputPages.value = '';
     inputRead.checked = false;
 });
 
 const libraryArr = [];
 
-function Book(title, author, genre, year, pages, haveRead){
+function Book(title, author, genre, year, pages, read){
     this.title = title;
     this.author = author;
     this.genre = genre;
     this.year = year;
     this.pages = pages;
-    this.haveRead = haveRead;
+    this.read = read;
 }
 
-function addNewBookToLibrary(title, author, genre, year, pages, haveRead){
-    const book = new Book(title, author, genre, year, pages, haveRead);
+function addNewBookToLibrary(title, author, genre, year, pages, read){
+    const book = new Book(title, author, genre, year, pages, read);
     libraryArr.push(book);
-    console.dir(book);
+    updateLibraryTable();
+}
+
+function updateLibraryTable(){
+    // clear library table
+    while(libraryTbody.firstChild)
+        libraryTbody.removeChild(libraryTbody.firstChild);
+
+    libraryArr.forEach(book => {
+        const newRow = document.createElement("tr");
+        for(const [key, value] of Object.entries(book)){
+            const newCell = document.createElement("td");
+            newCell.headers = `${key}-column`;
+
+            if(key === "read"){
+                const checkbox = document.createElement("input");
+                checkbox.type = "checkbox";
+                checkbox.addEventListener('change', () => book.read = !book.read);
+                newCell.appendChild(checkbox);
+                checkbox.checked = book.read;
+            }
+            else{
+                if(value === '')
+                    newCell.textContent = "N/A";
+                else {
+                    newCell.textContent = value;
+                }
+            }
+
+            newRow.appendChild(newCell);
+        }
+
+        // add a cell with a delete button
+        const removeCell = document.createElement("td");
+        removeCell.headers = "remove-column";
+        const removeButton = document.createElement("button");
+        removeButton.addEventListener('click', () => {
+            libraryArr.splice(libraryArr.indexOf(book), 1);
+            libraryTbody.removeChild(newRow)
+        });
+
+        removeButton.textContent = "Remove";
+        removeButton.classList.add("remove-btn");
+        removeCell.appendChild(removeButton);
+        newRow.appendChild(removeCell);
+
+        libraryTbody.appendChild(newRow);
+    });
 }
